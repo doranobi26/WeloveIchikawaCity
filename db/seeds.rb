5111cfc5d35c7eedfad29f1ceb99ea0ea2b4cdc8
vagrant <vagrant@localhost.localdomain>
2020-09-20 12:52:13 +0000

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,139 +10,135 @@ Area.create!(
 	[
 		{
 			name: "相之川",
-			postal_code: "272-0143"
+			postal_code: "2720143"
 		},
 		{
 			name: "新井",
-			postal_code: "272-0144"
+			postal_code: "2720144"
 		},
 		{
 			name: "伊勢宿",
-			postal_code: "272-0106"
+			postal_code: "2720106"
 		},
 		{
 			name: "市川",
-			postal_code: "272-0034"
+			postal_code: "2720034"
 		},
 		{
 			name: "市川南",
-			postal_code: "272-0033"
+			postal_code: "2720033"
 		},
 		{
 			name: "稲越町",
-			postal_code: "272-0831"
+			postal_code: "2720831"
 		},
 		{
 			name: "入船",
-			postal_code: "272-0134"
+			postal_code: "2720134"
 		},
 		{
 			name: "大洲",
-			postal_code: "272-0032"
+			postal_code: "2720032"
 		},
 		{
 			name: "大野町",
-			postal_code: "272-0805"
+			postal_code: "2720805"
 		},
 		{
 			name: "大野",
-			postal_code: "272-0801"
+			postal_code: "2720801"
 		},
 		{
 			name: "大和田",
-			postal_code: "272-0025"
+			postal_code: "2720025"
 		},
 		{
 			name: "押切",
-			postal_code: "272-0107"
+			postal_code: "2720107"
 		},
 		{
 			name: "鬼越",
-			postal_code: "272-0022"
+			postal_code: "2720022"
 		},
 		{
 			name: "鬼高",
-			postal_code: "272-0015"
+			postal_code: "2720015"
 		},
 		{
 			name: "欠真間",
-			postal_code: "272-0142"
+			postal_code: "2720142"
 		},
 		{
 			name: "柏井町",
-			postal_code: "272-0802"
+			postal_code: "2720802"
 		},
 		{
 			name: "加藤新田",
-			postal_code: "272-0124"
+			postal_code: "2720124"
 		},
 		{
-			name: "上妙典(1から999番地)",
-			postal_code: "272-0112"
-		},
-		{
-			name: "上妙典(その他)",
-			postal_code: "272-0012"
+			name: "上妙典",
+			postal_code: "2720012"
 		},
 		{
 			name: "河原",
-			postal_code: "272-0101"
+			postal_code: "2720101"
 		},
 		{
 			name: "香取",
-			postal_code: "272-0141"
+			postal_code: "2720141"
 		},
 		{
 			name: "北方",
-			postal_code: "272-0815"
+			postal_code: "2720815"
 		},
 		{
 			name: "北国分",
-			postal_code: "272-0836"
+			postal_code: "2720836"
 		},
 		{
 			name: "行徳駅前",
-			postal_code: "272-0133"
+			postal_code: "2720133"
 		},
 		{
 			name: "国府台",
-			postal_code:"272-0827"
+			postal_code:"2720827"
 		},
 		{
 			name: "高谷",
-			postal_code: "272-0013"
+			postal_code: "2720013"
 		},
 		{
 			name: "高谷新町",
-			postal_code: "272-0011"
+			postal_code: "2720011"
 		},
 		{
 			name: "国分",
-			postal_code: "272-0834"
+			postal_code: "2720834"
 		},
 		{
 			name: "幸",
-			postal_code: "272-0123"
+			postal_code: "2720123"
 		},
 		{
 			name: "塩浜",
-			postal_code: "272-0127"
+			postal_code: "2720127"
 		},
 		{
 			name: "塩焼",
-			postal_code: "272-0114"
+			postal_code: "2720114"
 		},
 		{
 			name: "島尻",
-			postal_code: "272-0145"
+			postal_code: "2720145"
 		},
 		{
 			name: "下貝塚",
-			postal_code: "272-0821"
+			postal_code: "2720821"
 		},
 		{
 			name: "下新宿",
-			postal_code: "272-0102"
+			postal_code: "2720102"
 		},
 		{
 			name: "下妙典",
@@ -150,175 +146,175 @@ Area.create!(
 		},
 		{
 			name: "新田",
-			postal_code: "272-0035"
+			postal_code: "2720035"
 		},
 		{
 			name: "末広",
-			postal_code: "272-0121"
+			postal_code: "2720121"
 		},
 		{
 			name: "菅野",
-			postal_code: "272-0824"
+			postal_code: "2720824"
 		},
 		{
 			name: "須和田",
-			postal_code: "272-0825"
+			postal_code: "2720825"
 		},
 		{
 			name: "関ケ島",
-			postal_code: "272-0105"
+			postal_code: "2720105"
 		},
 		{
 			name: "曽谷",
-			postal_code: "272-0832"
+			postal_code: "2720832"
 		},
 		{
 			name: "高石神",
-			postal_code: "272-0814"
+			postal_code: "2720814"
 		},
 		{
 			name: "高浜町",
-			postal_code: "272-0125"
+			postal_code: "2720125"
 		},
 		{
 			name: "宝",
-			postal_code: "272-0122"
+			postal_code: "2720122"
 		},
 		{
 			name: "田尻",
-			postal_code: "272-0014"
+			postal_code: "2720014"
 		},
 		{
 			name: "千鳥町",
-			postal_code: "272-0126"
+			postal_code: "2720126"
 		},
 		{
 			name: "稲荷木",
-			postal_code: "272-0024"
+			postal_code: "2720024"
 		},
 		{
 			name: "富浜",
-			postal_code: "272-0115"
+			postal_code: "2720115"
 		},
 		{
 			name: "中国分",
-			postal_code: "272-0835"
+			postal_code: "2720835"
 		},
 		{
 			name: "中山",
-			postal_code: "272-0813"
+			postal_code: "2720813"
 		},
 		{
 			name: "新浜",
-			postal_code: "272-0136"
+			postal_code: "2720136"
 		},
 		{
 			name: "原木",
-			postal_code: "272-0004"
+			postal_code: "2720004"
 		},
 		{
 			name: "東大和田",
-			postal_code: "272-0026"
+			postal_code: "2720026"
 		},
 		{
 			name: "東国分",
-			postal_code: "272-0833"
+			postal_code: "2720833"
 		},
 		{
 			name: "東菅野",
-			postal_code: "272-0823"
+			postal_code: "2720823"
 		},
 		{
 			name: "東浜",
-			postal_code: "272-0003"
+			postal_code: "2720003"
 		},
 		{
 			name: "日之出",
-			postal_code: "272-0135"
+			postal_code: "2720135"
 		},
 		{
 			name: "平田",
-			postal_code: "272-0031"
+			postal_code: "2720031"
 		},
 		{
 			name: "広尾",
-			postal_code: "272-0146"
+			postal_code: "2720146"
 		},
 		{
 			name: "福栄",
-			postal_code: "272-0137"
+			postal_code: "2720137"
 		},
 		{
 			name: "二俣",
-			postal_code: "272-0001"
+			postal_code: "2720001"
 		},
 		{
 			name: "二俣新町",
-			postal_code: "272-0002"
+			postal_code: "2720002"
 		},
 		{
 			name: "奉免町",
-			postal_code: "272-0803"
+			postal_code: "2720803"
 		},
 		{
 			name: "北方町",
-			postal_code: "272-0811"
+			postal_code: "2720811"
 		},
 		{
 			name: "堀之内",
-			postal_code: "272-0837"
+			postal_code: "2720837"
 		},
 		{
 			name: "本行徳",
-			postal_code: "272-0103"
+			postal_code: "2720103"
 		},
 		{
 			name: "本塩",
-			postal_code: "272-0104"
+			postal_code: "2720104"
 		},
 		{
 			name: "真間",
-			postal_code: "272-0826"
+			postal_code: "2720826"
 		},
 		{
 			name: "湊",
-			postal_code: "272-0131"
+			postal_code: "2720131"
 		},
 		{
 			name: "湊新田",
-			postal_code: "272-0132"
+			postal_code: "2720132"
 		},
 		{
 			name: "南大野",
-			postal_code: "272-0804"
+			postal_code: "2720804"
 		},
 		{
 			name: "南行徳",
-			postal_code: "272-0138"
+			postal_code: "2720138"
 		},
 		{
 			name: "南八幡",
-			postal_code: "272-0023"
+			postal_code: "2720023"
 		},
 		{
 			name: "宮久保",
-			postal_code: "272-0822"
+			postal_code: "2720822"
 		},
 		{
 			name: "妙典",
-			postal_code: "272-0111"
+			postal_code: "2720111"
 		},
 		{
 			name: "本北方",
-			postal_code: "272-0816"
+			postal_code: "2720816"
 		},
 		{
 			name: "八幡",
-			postal_code: "272-0021"
+			postal_code: "2720021"
 		},
 		{
 			name: "若宮",
-			postal_code: "272-0812"
+			postal_code: "2720812"
 		}
 
 	])
